ca73345c6b7d57f8ed43c86d5545f04f4f347ae1
Blake Campbell <blakecampbell.dev@gmail.com>
2017-01-22 15:08:34 -0800

    Got basic chart set up and working

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h3>{{ msg }}</h3>
+    <canvas id="myChart" width="400" height="400"></canvas>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
@@ -8,19 +9,52 @@
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
       <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <button aria-hidden="true" v-on:click="update()"></button>
     </ul>
   </div>
 </template>
 
 <script>
+import Chart from 'chart.js'
+
 export default {
   name: 'hello',
   data () {
     return {
       msg: 'There\'s 168 Hours in a week. Minus six hours of sleep a night. You are left with 126 hours, make them count.'
     }
-  }
+  },
+  methods: {
+    update: function () {
+      var ctx = document.getElementById('myChart')
+      var data = {
+        labels: [
+          'Time Passed',
+          'Time Left'
+        ],
+        datasets: [
+          {
+            data: [18, 126],
+            backgroundColor: [
+              '#FF6384',
+              '#36A2EB'
+            ],
+            hoverBackgroundColor: [
+              '#FF6384',
+              '#36A2EB'
+            ]
+          }
+        ]
+      }
+      var myDoughnutChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: data,
+        options: {}
+      })
+      console.log(myDoughnutChart)
+    }
+  },
+  created: function () {}
 }
 </script>
 
